e3728ed08077b8dea7893ba4c76612775011e296
Cabriada Castilla <alejandro.cabriada@stefanini.com>
2024-09-12 12:48:01 +0200

    compose

--- backend/train_fasttext_model.py
+++ backend/train_fasttext_model.py
@@ -5,17 +5,9 @@ from app.utils import get_preprocessed_texts
 
 load_dotenv()
 
-
 if __name__ == "__main__":
     
     model_path = os.getenv("FASTTEXT_MODEL_PATH")
     config_path = os.getenv("FASTTEXT_CONFIG_PATH")
     
     train_fasttext(model_path, get_preprocessed_texts, config_path)
-
-
-
-    # Esta llamando al main 
-    #Existe el siguiente problema al instanciar
-    #llegamos a self.embedings  y el primer modelo esta entrenado , self.trainer entrena el modelo como el main
-    #
